6d708ec31b0b634973b4c5a61dc9c2c3dce92dc0
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-05-06 22:05:20 +0530

    Algo SQL practices

--- MYSQL/sql_practice.sql
+++ MYSQL/sql_practice.sql
@@ -25,3 +25,22 @@ FROM
     occupations
 GROUP BY occupation
 ORDER BY COUNT(occupation) , occupation;
+/* Problem 2 :- 
+Consider P1 (a, c) and P2 (b, d) to be two points on a 2D plane where (a, b) are the respective minimum and maximum values 
+of Northern Latitude (LAT_N) and (c, d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits.
+The STATION table has following columns:
+-----
+STATION 
+----
+Field Туре
+ID    NUMBER 
+CITY  VARCHAR2(21)
+STATE VARCHAR2(2)
+LAT_N NUMBER
+LONG_W NUMBER
+*/
+SELECT 
+    FORMAT(SQRT(POW(MAX(LAT_N) - MIN(LAT_N), 2) + POW(MAX(LONG_W) - MIN(LONG_W), 2)),4)
+FROM
+    station;
