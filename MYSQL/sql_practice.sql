8aefcb5ab7cefe537b08296bce02d941c95c14c5
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-05-08 14:41:53 +0530

    SQL Basic self join with datediff

--- MYSQL/sql_practice.sql
+++ MYSQL/sql_practice.sql
@@ -52,6 +52,34 @@ FROM
     teacher
 GROUP BY teacher_id;
 
+/*
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+*/
+SELECT 
+    w1.id AS id
+FROM
+    weather w1
+        INNER JOIN
+    weather w2 ON (DATEDIFF(w1.recordDate, w2.recordDate) = 1)
+WHERE
+    w1.temperature > w2.temperature;
+
 
 -- Advanced Select
 /* Problem 1 :- Generate folowing result sets :- 
