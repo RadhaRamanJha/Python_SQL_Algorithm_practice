920981d2a034b4165de0f20ab3381f6b89fa609f
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-05-12 23:53:29 +0530

    SQL Practice - Joins

--- MYSQL/sql_practice.sql
+++ MYSQL/sql_practice.sql
@@ -202,6 +202,38 @@ FROM
 WHERE
     w1.temperature > w2.temperature;
     
+/* Aggregate Functions :- 
+Table: Transactions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| country       | varchar |
+| state         | enum    |
+| amount        | int     |
+| trans_date    | date    |
++---------------+---------+
+id is the primary key of this table.
+The table has information about incoming transactions.
+The state column is an enum of type ["approved", "declined"].
+ 
+
+Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+
+Return the result table in any order.
+*/
+SELECT 
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(IF(state = 'approved', 1, 0)) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM
+    transactions
+GROUP BY month , country;
+    
 /* 
 Table: Employee
 
