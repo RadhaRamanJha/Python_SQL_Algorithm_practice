5545aedc92b3d54bc0bbba30c988fbe1848ec9fc
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-05-19 16:08:46 +0530

    Aggregate queries for not null columns

--- MYSQL/sql_practice.sql
+++ MYSQL/sql_practice.sql
@@ -609,3 +609,47 @@ select contest_id,round((count(user_id)*100/users_no),2) as percentage
 from Register,total_user
 group by contest_id
 order by percentage desc,contest_id;
+
+/*
+Table: Queries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| query_name  | varchar |
+| result      | varchar |
+| position    | int     |
+| rating      | int     |
++-------------+---------+
+This table may have duplicate rows.
+This table contains information collected from some queries on a database.
+The position column has a value from 1 to 500.
+The rating column has a value from 1 to 5. Query with rating less than 3 is a poor query.
+ 
+
+We define query quality as:
+
+The average of the ratio between query rating and its position.
+
+We also define poor query percentage as:
+
+The percentage of all queries with rating less than 3.
+
+Write a solution to find each query_name, the quality and poor_query_percentage.
+
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+
+Return the result table in any order. */
+SELECT 
+    query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(SUM(CASE
+                WHEN rating < 3 THEN 1
+                ELSE 0
+            END) * 100 / COUNT(*),
+            2) AS poor_query_percentage
+FROM
+    queries
+WHERE
+    query_name IS NOT NULL
+GROUP BY query_name;
