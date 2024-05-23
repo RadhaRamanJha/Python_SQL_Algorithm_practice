64931fc182cc93175f4870acaed22c6906e11967
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-05-23 15:12:03 +0530

    Biggest Single number -- Leet code

--- MYSQL/sql_practice.sql
+++ MYSQL/sql_practice.sql
@@ -657,6 +657,7 @@ GROUP BY query_name;
 show databases;
 use newdb;
 show tables;
+
 /*
 Table: Courses
 
@@ -679,6 +680,7 @@ FROM
     courses
 GROUP BY class
 HAVING COUNT(student) >= 5;
+
 /*
 Table: Followers
 
@@ -701,3 +703,27 @@ FROM
     followers
 GROUP BY user_id
 ORDER BY user_id;
+
+/*
+Table: MyNumbers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+This table may contain duplicates (In other words, there is no primary key for this table in SQL).
+Each row of this table contains an integer.
+ 
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+Find the largest single number. If there is no single number, report null. */
+select max(num) as num
+from (
+    select num
+    from MyNumbers
+    group by num
+    having count(num) = 1
+) count_num;
+
