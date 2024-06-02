8f243d2125db4779855a5e4bcd1e8f8b69982ae7
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-06-02 16:04:30 +0530

    Query manhatan distance between two point and round the distance to four decimal places

--- MYSQL/sql_practice.sql
+++ MYSQL/sql_practice.sql
@@ -784,4 +784,15 @@ FROM
     employee ON manager.company_code = employee.company_code
 GROUP BY company.company_code , company.founder
 ORDER BY company.company_code;
+/*
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+ a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.*/
+SELECT 
+    ABS(FORMAT(MAX(lat_n) - MIN(lat_n), 4)) + ABS(FORMAT(MAX(long_w) - MIN(long_w), 4))
+FROM
+    station;
 
