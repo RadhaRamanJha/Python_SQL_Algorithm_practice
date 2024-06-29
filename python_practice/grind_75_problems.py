d8a875b914118bc74a87cf735e03902d93475651
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-06-29 17:17:06 +0530

    Grind 75 - Best time to buy and sell stocks

--- python_practice/grind_75_problems.py
+++ python_practice/grind_75_problems.py
@@ -55,3 +55,18 @@ class Solution:
         elif chk_str != chk_str[::-1]:
             return False
     
+    # 4. You are given an array prices where prices[i] is the price of a given stock on the ith day.
+    # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+    # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)-1
+        current_max, max_profit, current_profit = 0, 0, 0
+        for i in range(n,-1,-1):
+            if prices[i] > current_max:
+                current_max = prices[i]
+            if prices[i] < current_max:
+                current_profit = current_max - prices[i]
+            if current_profit > max_profit:
+                max_profit = current_profit
+        return max_profit
