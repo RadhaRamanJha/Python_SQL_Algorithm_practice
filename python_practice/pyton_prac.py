822170a6b73396aad8057a32e415b0d354dcc7ac
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-07-08 14:44:52 +0530

    Reduce Function -- Hacker Rank

--- python_practice/pyton_prac.py
+++ python_practice/pyton_prac.py
@@ -493,3 +493,31 @@ def fibonacci(n):
 if __name__ == '__main__':
     n = int(input())
     print(list(map(cube, fibonacci(n))))
+
+"""
+Input Format
+
+First line contains n, the number of rational numbers.
+The ith of next n lines contain two integers each, the numerator( Ni ) and denominator( Di ) of the  rational number in the list.
+
+Constraints
+1 <= n <= 100
+1 <= Ni, Di <= 10**9
+Output Format
+
+Print only one line containing the numerator and denominator of the product of the numbers in the list in its simplest form, i.e. numerator and denominator have no common divisor other than 1.
+
+"""
+from fractions import Fraction
+from functools import reduce
+
+def product(fracs):
+    t = reduce(lambda x, y: x*y, fracs, 1)
+    return t.numerator, t.denominator
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
