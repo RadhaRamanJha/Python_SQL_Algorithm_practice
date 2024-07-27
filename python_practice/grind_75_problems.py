0d0c402e9769552242ca4e684b9896d0395ff3d3
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-07-27 08:53:44 +0530

    Invert Binary Tree -- Grind 75

--- python_practice/grind_75_problems.py
+++ python_practice/grind_75_problems.py
@@ -182,3 +182,20 @@ class MyQueue:
             return True
         else:
             return False
+        
+## Given the root of a binary tree, invert the tree, and return its root.     
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is not None:
+            temp = root.left
+            root.left = root.right
+            root.right = temp
+            Solution.invertTree(self, root.left)
+            Solution.invertTree(self, root.right)
+        return root
