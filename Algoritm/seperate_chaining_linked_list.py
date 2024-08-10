1d3b6f974c0403c6a578763f613128512bfdb6e6
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-08-10 21:19:02 +0530

    remove first node of a linked list

--- Algoritm/seperate_chaining_linked_list.py
+++ Algoritm/seperate_chaining_linked_list.py
@@ -31,3 +31,23 @@ class HashTable():
 
         self.table[hash_code] = LinkedEntry(key, value, self.table[hash_code])
         self.key_counts += 1
+
+    def remove(self,key):
+        hash_code = hash(key) % self.size
+        entry = self.table[hash_code]                     # self.table[hash_code] - refers the first entry in linked list assosiated with hash code
+        prev = None
+        while entry:                                      # Iterate as long as there are entries in the Linked List
+            if entry.key == key:                          # Locate entry by comparing target key with key field of entry
+                if prev:
+                    prev.next = entry.next                # When found, if there is prev reference, link around entry hence removing it from Linked List
+                else:
+                    self.table[hash_code] = entry.next    # set the linked list first entry to second node in Linked List
+    
+                self.key_counts -= 1                      # Decrement count of entries, N.
+                return entry.value                        # Return the value of assosiated with the entry being removed
+        
+            prev, entry = entry, entry.next               # if key is not found continue iterating by setting prev to entry and entry to entry.next
+
+        return None
+
+      
