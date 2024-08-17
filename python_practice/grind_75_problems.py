59b0626d713258ccfe0cedf7ac34c37a9f3c7eb4
Radha Raman Jha <radharamanjha1989@gmail.com>
2024-08-17 16:21:32 +0530

    Linked List Cycle - Grind 75 Problem

--- python_practice/grind_75_problems.py
+++ python_practice/grind_75_problems.py
@@ -249,3 +249,27 @@ class MyQueue:
                 ll_toEnd = ll_toEnd.next
                 ll_toMid = ll_toMid.next
         return ll_toMid
+    
+# Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+#  Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+# Return true if there is a cycle in the linked list. Otherwise, return false.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        first_pointer = head
+        if first_pointer is None:
+            return False
+        first_pointer, second_pointer = head.next, head
+        while first_pointer != second_pointer:
+            if first_pointer is None or first_pointer.next is None:
+                return False
+            first_pointer = first_pointer.next.next
+            second_pointer = second_pointer.next
+        return True
